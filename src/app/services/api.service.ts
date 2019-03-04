d1a64b2ea80b8da08824aca93b4d33e6c024e024
Paweł <illmoded@gmail.com>
2019-03-04 12:42:38 +0100

    add placeholders for install script

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {NodeService} from "./node.service";
-import {AppSettingsService} from "./app-settings.service";
+import { HttpClient } from "@angular/common/http";
+import { NodeService } from "./node.service";
+import { AppSettingsService } from "./app-settings.service";
 
 @Injectable()
 export class ApiService {
 
-  rpcUrl = `https://my.nos.cash/api/node-api`;
-  // rpcUrl = `http://localhost:9950/api/node-api`;
+  rpcUrl = `PLACEHOLDER`;
 
   constructor(private http: HttpClient, private node: NodeService, private appSettings: AppSettingsService) { }
 
@@ -30,27 +29,27 @@ export class ApiService {
       });
   }
 
-  async accountsBalances(accounts: string[]): Promise<{balances: any }> {
+  async accountsBalances(accounts: string[]): Promise<{ balances: any }> {
     return await this.request('accounts_balances', { accounts });
   }
-  async accountsFrontiers(accounts: string[]): Promise<{frontiers: any }> {
+  async accountsFrontiers(accounts: string[]): Promise<{ frontiers: any }> {
     return await this.request('accounts_frontiers', { accounts });
   }
-  async accountsPending(accounts: string[], count: number = 50): Promise<{blocks: any }> {
+  async accountsPending(accounts: string[], count: number = 50): Promise<{ blocks: any }> {
     return await this.request('accounts_pending', { accounts, count, source: true });
   }
   async delegatorsCount(account: string): Promise<{ count: string }> {
     return await this.request('delegators_count', { account });
   }
   async representativesOnline(): Promise<{ representatives: any }> {
-    return await this.request('representatives_online', { });
+    return await this.request('representatives_online', {});
   }
 
-  async blocksInfo(blocks): Promise<{blocks: any, error?: string}> {
+  async blocksInfo(blocks): Promise<{ blocks: any, error?: string }> {
     return await this.request('blocks_info', { hashes: blocks, pending: true, source: true });
   }
-  async blockCount(): Promise<{count: number, unchecked: number }> {
-    return await this.request('block_count', { });
+  async blockCount(): Promise<{ count: number, unchecked: number }> {
+    return await this.request('block_count', {});
   }
   async workGenerate(hash): Promise<{ work: string }> {
     return await this.request('work_generate', { hash });
@@ -58,13 +57,13 @@ export class ApiService {
   async process(block): Promise<{ hash: string, error?: string }> {
     return await this.request('process', { block: JSON.stringify(block) });
   }
-  async accountHistory(account, count = 25, raw = false): Promise<{history: any }> {
+  async accountHistory(account, count = 25, raw = false): Promise<{ history: any }> {
     return await this.request('account_history', { account, count, raw });
   }
   async accountInfo(account): Promise<any> {
     return await this.request('account_info', { account, pending: true, representative: true, weight: true });
   }
-  async validateAccountNumber(account): Promise<{ valid: '1'|'0' }> {
+  async validateAccountNumber(account): Promise<{ valid: '1' | '0' }> {
     return await this.request('validate_account_number', { account });
   }
   async pending(account, count): Promise<any> {
